5ab83a24e7fc84d5f9f9d9dba7931a44dbd36951
최장호 <hmchoi651@naver.com>
2020-05-19 19:26:47 +0900

    [#7] Feat : webpack , babel config 설정

---
+++ FE/airbnb/webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 3000,
+    hot: false,
+    publicPath: "/",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", "jsx", ".ts", ".tsx"],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "public/index_dev.html",
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, "./.env.development"),
+    }),
+  ],
+};
