a1e6ea24f161a3ccc41919f92d86ad23837f5301
최장호 <hmchoi651@naver.com>
2020-05-26 21:26:04 +0900

    [#16] Docs : 디버깅용 코드 일부 주석처리

--- FE/airbnb/src/components/rooms/Rooms.jsx
+++ FE/airbnb/src/components/rooms/Rooms.jsx
@@ -4,9 +4,9 @@ import { fetchInitialData } from "../../modules/roomsList.js";
 import { useDispatch } from "react-redux";
 
 const Rooms = () => {
-  //const dispatch = useDispatch();
-  useFetch("http://15.165.117.230/api/mock/rooms");
-  // fetchInitialData, dispatch
+  // const dispatch = useDispatch();
+  // useFetch("http://15.165.117.230/api/mock/rooms", fetchInitialData, dispatch);
+
   return <div></div>;
 };
 
