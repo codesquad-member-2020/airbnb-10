b7f7152ce8ebb8c9464d59326188e540cdea54a6
Elllin <noto01@naver.com>
2020-05-29 13:27:41 +0900

    [#29] feat: rooms title 스타일 추가, fetch data 구조분해할당
    폰트사이즈및 layout

--- FE/airbnb/src/components/rooms/Rooms.jsx
+++ FE/airbnb/src/components/rooms/Rooms.jsx
@@ -1,36 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch.jsx";
 import { fetchInitialData } from "../../modules/roomsList.js";
 import { useDispatch, useSelector } from "react-redux";
 
+import styled from "styled-components";
+
+const getDate = (tomorrow) => {
+  const today = new Date();
+  if (tomorrow) today.setDate(today.getDate() + 1);
+
+  let day = today.getDate();
+  let month = today.getMonth() + 1;
+  const year = today.getFullYear();
+
+  if (day < 10) day = `0${day}`;
+  if (month < 10) month = `0${month}`;
+
+  return `${year}-${month}-${day}`;
+};
+
+const getInitialUrl = () => {
+  const today = getDate();
+  const tomorrow = getDate(true);
+  const initialUrl = `http://15.165.117.230/api/mock/rooms?checkIn=${today}&checkOut=${tomorrow}`;
+  return initialUrl;
+};
+
 const Rooms = () => {
   const dispatch = useDispatch();
-
-  const getDate = (tomorrow) => {
-    const today = new Date();
-    if (tomorrow) today.setDate(today.getDate() + 1);
-
-    let day = today.getDate();
-    let month = today.getMonth() + 1;
-    const year = today.getFullYear();
-
-    if (day < 10) day = `0${day}`;
-    if (month < 10) month = `0${month}`;
-
-    return `${year}-${month}-${day}`;
-  };
-
-  const getInitialUrl = () => {
-    const today = getDate();
-    const tomorrow = getDate(true);
-    const initialUrl = `http://15.165.117.230/api/mock/rooms?checkIn=${today}&checkOut=${tomorrow}`;
-    return initialUrl;
-  };
+  const [totalCount, setTotalCount] = useState(null);
 
   useFetch(getInitialUrl(), fetchInitialData, dispatch);
-  const { content } = useSelector((state) => state.roomsListReducer);
 
-  return <div></div>;
+  const {
+    content: { total, accommodations },
+  } = useSelector((state) => state.roomsListReducer);
+
+  useEffect(() => {
+    setTotalCount(total);
+  }, [total]);
+
+  return (
+    <RoomsWrap>
+      <Title>
+        <div>{totalCount}개 이상의 숙소</div>
+      </Title>
+    </RoomsWrap>
+    // <div>
+    //  {accommodations.map(roomsList => )}
+    //  </div>
+  );
 };
+
+const RoomsWrap = styled.div`
+  margin: 40px 30px;
+`;
+
+const Title = styled.div`
+  padding: 10px 0;
+  font-size: 19px;
+`;
 
 export default Rooms;
