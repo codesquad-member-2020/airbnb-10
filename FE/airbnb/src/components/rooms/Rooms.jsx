d0b63bd27877203934a5abfd0572bfc26934ad04
Elllin <noto01@naver.com>
2020-05-29 12:50:25 +0900

    [#29] feat: 초기 url에 오늘, 내일 날짜 파라미터로 전달
    오늘, 내일 날짜 구하는 함수구현
    구한 날짜를 파라미터에 전달

--- FE/airbnb/src/components/rooms/Rooms.jsx
+++ FE/airbnb/src/components/rooms/Rooms.jsx
@@ -5,11 +5,29 @@ import { useDispatch, useSelector } from "react-redux";
 
 const Rooms = () => {
   const dispatch = useDispatch();
-  useFetch(
-    "http://15.165.117.230/api/mock/rooms?checkIn=2020-05-28&checkOut=2020-05-29",
-    fetchInitialData,
-    dispatch,
-  );
+
+  const getDate = (tomorrow) => {
+    const today = new Date();
+    if (tomorrow) today.setDate(today.getDate() + 1);
+
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    if (day < 10) day = `0${day}`;
+    if (month < 10) month = `0${month}`;
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const getInitialUrl = () => {
+    const today = getDate();
+    const tomorrow = getDate(true);
+    const initialUrl = `http://15.165.117.230/api/mock/rooms?checkIn=${today}&checkOut=${tomorrow}`;
+    return initialUrl;
+  };
+
+  useFetch(getInitialUrl(), fetchInitialData, dispatch);
   const { content } = useSelector((state) => state.roomsListReducer);
 
   return <div></div>;
