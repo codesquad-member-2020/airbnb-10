5f0d6e991024b84f972b6a1a9e217f99089d6d9d
Elllin <noto01@naver.com>
2020-06-01 17:33:39 +0900

    [#39] feat: 예약버튼 클릭 시 그림자 처리

--- FE/airbnb/src/components/reservation/Reservation.jsx
+++ FE/airbnb/src/components/reservation/Reservation.jsx
@@ -5,7 +5,12 @@ import styled from "styled-components";
 
 const Reservation = () => {
   const { isClicked } = useSelector((state) => state.reservationReducer);
-  return <ReservationWrap isClicked={isClicked}>123</ReservationWrap>;
+  return (
+    <>
+      <ReservationWrap isClicked={isClicked}>123</ReservationWrap>
+      {isClicked && <ModalShadow />}
+    </>
+  );
 };
 
 const ReservationWrap = styled.div`
@@ -19,6 +24,15 @@ const ReservationWrap = styled.div`
   width: 300px;
   z-index: 1;
   background-color: var(--white);
+`;
+
+const ModalShadow = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
 `;
 
 export default Reservation;
