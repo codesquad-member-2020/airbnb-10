38e7e9469fa34dde55cfb1fc18930aa7276be8c2
Jay <beginin15@gmail.com>
2020-06-02 02:44:45 +0900

    [#27] test: FilterServiceTest -> AccommodationDAOTest로 변경
    - 기존 FilterServiceTest에서 수행하는 테스트는 서비스 층과는 크게 관련이 없어서 DAO 테스트로 변경함
    
    - 테스트 코드 때문에 분리한 FilterService.findUsingFilter 메소드 제거

--- BE/src/test/java/com/codesquad/airbnb/dao/AccommodationDAOTest.java
+++ BE/src/test/java/com/codesquad/airbnb/dao/AccommodationDAOTest.java
@@ -1,8 +1,9 @@
-package com.codesquad.airbnb.business;
+package com.codesquad.airbnb.dao;
 
 import com.codesquad.airbnb.domain.model.Accommodation;
 import com.codesquad.airbnb.domain.model.Filter;
 import com.codesquad.airbnb.util.CurrencyConvertor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,21 +11,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class FilterServiceTest {
+public class AccommodationDAOTest {
 
-    private static final Logger log = LoggerFactory.getLogger(FilterServiceTest.class);
+    private static final Logger log = LoggerFactory.getLogger(AccommodationDAOTest.class);
 
     @Autowired
-    private FilterService filterService;
+    private AccommodationDAO accommodationDAO;
 
-    @Test
-    public void 숙소_필터링() throws Exception {
+    private Filter filter;
+    private Map<String, Object> parameters;
+
+    @BeforeEach
+    public void setParameters() {
         // given
-        Filter filter = new Filter();
+        filter = new Filter();
         // 1. 날짜 조건
         filter.setCheckIn(LocalDate.parse("2020-05-23"));
         filter.setCheckOut(LocalDate.parse("2020-05-24"));
@@ -37,8 +44,19 @@ public class FilterServiceTest {
         // 4. 페이징
         filter.setItemsOffset(0);
 
+        parameters = new HashMap<>();
+        parameters.put("people", filter.getPeople());
+        parameters.put("priceMin", filter.getPriceMin());
+        parameters.put("priceMax", filter.getPriceMax());
+        parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
+        parameters.put("period", filter.getPeriod());
+        parameters.put("itemsOffset", filter.getItemsOffset());
+    }
+
+    @Test
+    public void 숙소_필터링() throws Exception {
         // when
-        List<Accommodation> accommodations = filterService.findUsingFilter(filter);
+        List<Accommodation> accommodations = accommodationDAO.findUsingFilter(parameters);
 
         // then
         assertThat(accommodations).isNotNull();
@@ -49,6 +67,15 @@ public class FilterServiceTest {
         // 인원 검사
         assertThat(accommodations).allMatch(a -> a.getMaximumCapacity() >= filter.getPeople());
         // 페이징 검사
-        assertThat(accommodations.size()).isEqualTo(4);
+        assertThat(accommodations.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void 숙소_필터링_총_개수() throws Exception {
+        // when
+        int total = accommodationDAO.countOfFilterResult(parameters);
+
+        // then
+        assertThat(total).isEqualTo(2);
     }
 }
