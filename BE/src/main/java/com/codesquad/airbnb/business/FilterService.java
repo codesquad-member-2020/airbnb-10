c152501cb8ea9e9050d5125ffeafa937420624a0
Jay <beginin15@gmail.com>
2020-06-02 01:16:36 +0900

    [#27] feat: 필터링이 적용된 숙소 조회와 관련된 서비스 로직
    - CurrencyConvertor.java: 환율 계산을 위한 클래스. 추후 시간이 남으면 현재 환율 정보를 알려주는 외부 api와 배치 작업으로 환율 변동을 구현하고 싶음
    
    - FilterService.findUsingFilter 메소드: 테스트 코드에서 DTO를 맵핑하기 전 도메인 클래스 내용을 기반으로 테스트가 필요하여 메소드로 구분함

--- BE/src/main/java/com/codesquad/airbnb/business/FilterService.java
+++ BE/src/main/java/com/codesquad/airbnb/business/FilterService.java
@@ -1,21 +1,49 @@
 package com.codesquad.airbnb.business;
 
-import com.codesquad.airbnb.domain.model.Accommodation;
 import com.codesquad.airbnb.dao.AccommodationDAO;
+import com.codesquad.airbnb.domain.dto.AccommodationDTO;
+import com.codesquad.airbnb.domain.model.Accommodation;
+import com.codesquad.airbnb.domain.model.Filter;
+import com.codesquad.airbnb.util.CurrencyConvertor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FilterService {
 
+    private static final Logger log = LoggerFactory.getLogger(FilterService.class);
     private AccommodationDAO accommodationDAO;
 
     public FilterService(AccommodationDAO accommodationDAO) {
         this.accommodationDAO = accommodationDAO;
     }
 
-    public List<Accommodation> findAll() {
-        return accommodationDAO.findAll();
+    public List<AccommodationDTO> getAccommodations(Filter filter) {
+        return findUsingFilter(filter)
+                .stream()
+                .map(model -> new AccommodationDTO.Builder(model.getId())
+                        .name(model.getName())
+                        .pricePerNight(CurrencyConvertor.calculateCurrency(model.getPricePerNight()))
+                        .pricePerNightDiscounted(CurrencyConvertor.calculateCurrency(model.getPricePerNightDiscounted()))
+                        .totalPrice(CurrencyConvertor.calculateCurrency(model.getTotalPrice() * filter.getPeriod()))
+                        .isSuperHost(model.isSuperHost())
+                        .city(model.getCity().getName())
+                        .scoresRating(model.getScoresRating())
+                        .images(model.getImages())
+                        .build()).collect(Collectors.toList());
+    }
+
+    public List<Accommodation> findUsingFilter(Filter filter) {
+        log.debug("period: {}", filter.getPeriod());
+        return accommodationDAO.findUsingFilter(filter.getPeople(),
+                filter.getPriceMin(),
+                filter.getPriceMax(),
+                CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW,
+                filter.getPeriod(),
+                filter.getItemsOffset());
     }
 }
