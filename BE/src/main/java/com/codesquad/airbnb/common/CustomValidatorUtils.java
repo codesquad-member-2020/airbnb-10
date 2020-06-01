195df29ee90f7a812acaa849061247a6f0c8990e
Jay <beginin15@gmail.com>
2020-06-02 01:03:31 +0900

    [#27] refactor: 유효성 검증 관련 유틸 클래스 정의
    - BindingResult에 저장된 오류 메세지를 하나의 문자열로 반환 (response 응답용)

---
+++ BE/src/main/java/com/codesquad/airbnb/common/CustomValidatorUtils.java
@@ -0,0 +1,13 @@
+package com.codesquad.airbnb.common;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import java.util.stream.Collectors;
+
+public class CustomValidatorUtils {
+
+    public static String getErrorMessage(BindingResult result) {
+        return result.getAllErrors().stream().map(ObjectError::getCode).collect(Collectors.joining(", "));
+    }
+}
