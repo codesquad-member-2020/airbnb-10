8ea2e03bb0507c4b6c9da89cae895e080b91c1c4
Jay <beginin15@gmail.com>
2020-06-03 00:18:54 +0900

    [#45] test: 예약 모달창 api에서 사용하는 dao 메소드가 반환하는 데이터의 정확성 테스트
    - 필터링 api에서 가져온 특정 숙소와 해당 숙소의 예약 모달창 요금 데이터를 비교하는 테스트 코드도 추가

--- BE/src/test/java/com/codesquad/airbnb/dao/AccommodationDAOTest.java
+++ BE/src/test/java/com/codesquad/airbnb/dao/AccommodationDAOTest.java
@@ -25,8 +25,14 @@ public class AccommodationDAOTest {
     @Autowired
     private AccommodationDAO accommodationDAO;
 
+    @Autowired
+    private FeePolicyMapper feePolicyMapper;
+
     private Filter filter;
+
     private Map<String, Object> parameters;
+
+    private float accommodationTaxRate;
 
     @BeforeEach
     public void setParameters() {
@@ -51,19 +57,22 @@ public class AccommodationDAOTest {
         parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
         parameters.put("period", filter.getPeriod());
         parameters.put("itemsOffset", filter.getItemsOffset());
+
+        accommodationTaxRate = feePolicyMapper.findAccommodationTax();
     }
 
     @Test
-    public void 숙소_필터링() throws Exception {
+    public void 숙소_필터링() {
+
         // when
         List<Accommodation> accommodations = accommodationDAO.findUsingFilter(parameters);
 
         // then
         assertThat(accommodations).isNotNull();
         // 최소 요금 검사
-        assertThat(accommodations).allMatch(a -> (CurrencyConvertor.calculateCurrency(a.getTotalPrice())) >= filter.getPriceMin());
+        assertThat(accommodations).allMatch(a -> (a.getPricePerNightDiscounted(accommodationTaxRate)) >= filter.getPriceMin());
         // 최대 요금 검사
-        assertThat(accommodations).allMatch(a -> (CurrencyConvertor.calculateCurrency(a.getTotalPrice())) <= filter.getPriceMax());
+        assertThat(accommodations).allMatch(a -> (a.getPricePerNightDiscounted(accommodationTaxRate)) <= filter.getPriceMax());
         // 인원 검사
         assertThat(accommodations).allMatch(a -> a.getMaximumCapacity() >= filter.getPeople());
         // 페이징 검사
@@ -71,11 +80,71 @@ public class AccommodationDAOTest {
     }
 
     @Test
-    public void 숙소_필터링_총_개수() throws Exception {
+    public void 숙소_필터링_총_개수() {
         // when
         int total = accommodationDAO.countOfFilterResult(parameters);
 
         // then
         assertThat(total).isEqualTo(2);
+    }
+
+    @Test
+    public void 숙소_예약_모달창() {
+        // given
+        parameters.put("id", 2);
+
+        // when
+        Accommodation accommodation = accommodationDAO.findAccommodationChargeInfoById(parameters);
+
+        // then
+        assertThat(accommodation.getId()).isEqualTo(2);
+        assertThat(accommodation).isNotNull();
+        assertThat(accommodation.getOriginalPricePerNight()).isEqualTo(116996);
+        assertThat(accommodation.getDiscountedPricePerNight()).isEqualTo(116996);
+        assertThat(accommodation.getCleaningFee()).isEqualTo(0);
+        assertThat(accommodation.getServiceTax(filter)).isEqualTo(22847);
+        assertThat(accommodation.getAccommodationTax(filter, accommodationTaxRate)).isEqualTo(2284);
+        assertThat(accommodation.getTotalPrice(filter, accommodationTaxRate)).isEqualTo(142127);
+    }
+
+    @Test
+    public void 숙소_필터링과_예약_모달창_비교() {
+        // given
+        filter = new Filter();
+        // 1. 날짜 조건
+        filter.setCheckIn(LocalDate.parse("2020-05-23"));
+        filter.setCheckOut(LocalDate.parse("2020-05-25"));
+        // 2. 페이징
+        filter.setItemsOffset(0);
+
+        parameters = new HashMap<>();
+        parameters.put("people", filter.getPeople());
+        parameters.put("priceMin", filter.getPriceMin());
+        parameters.put("priceMax", filter.getPriceMax());
+        parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
+        parameters.put("period", filter.getPeriod());
+        parameters.put("itemsOffset", filter.getItemsOffset());
+        // when
+        List<Accommodation> accommodations = accommodationDAO.findUsingFilter(parameters);
+        Integer id = 1;
+        parameters.put("id", id);
+        Accommodation accommodationForBooking = accommodationDAO.findAccommodationChargeInfoById(parameters);
+
+        // then
+        Accommodation accommodationFiltered = null;
+        for (Accommodation a : accommodations) {
+            if (a.getId() == id) {
+                accommodationFiltered = a;
+            }
+        }
+
+        assertThat(accommodationForBooking.getOriginalPricePerNight()).isEqualTo(accommodationFiltered.getOriginalPricePerNight());
+        assertThat(accommodationForBooking.getDiscountedPricePerNight()).isEqualTo(accommodationFiltered.getDiscountedPricePerNight());
+        assertThat(accommodationForBooking.getCleaningFee()).isEqualTo(accommodationFiltered.getCleaningFee());
+        assertThat(accommodationForBooking.getServiceTax(filter)).isEqualTo(accommodationFiltered.getServiceTax(filter));
+        assertThat(accommodationForBooking.getAccommodationTax(filter, accommodationTaxRate)).isEqualTo(accommodationFiltered.getAccommodationTax(filter, accommodationTaxRate));
+        assertThat(accommodationForBooking.getOriginalPricePerNight()).isEqualTo(accommodationFiltered.getOriginalPricePerNight());
+        assertThat(accommodationForBooking.getDiscountedPricePerNight()).isEqualTo(accommodationFiltered.getDiscountedPricePerNight());
+        assertThat(accommodationForBooking.getTotalPrice(filter, accommodationTaxRate)).isEqualTo(accommodationFiltered.getTotalPrice(filter, accommodationTaxRate));
     }
 }
