a0cbb7a0f527f2666327c37e58368cbf2f6698a4
Jay <beginin15@gmail.com>
2020-06-03 00:15:15 +0900

    [#45] refactor: DAOUtils와 Accommodation 도메인 클래스의 변경으로 인한 FilterService 수정
    - 쿼리의 인자로 전달되는 parameters를 생성하는 로직이 DAOUtils 클래스로 이동하여 기존 메소드 제거
    
    - DB에서 환율이 적용된 가격이 가져오므로 환율을 계산하는 유틸 클래스(CurrencyConvertor) 메소드 호출이 필요없어짐.
    
    - Accommodation 도메인 클래스의 필드 변경으로 인해 pricePerNight, pricePerNightDiscounted, totalPrice 계산을 위해서 가져오는 데이터가 달라짐
    
    - CurrencyConvertor.java: 환율을 적용하는 로직이 DB로 이동하여 필요없어진 메소드 제거. 이후 환율 비율(EXCHANGE_RATE_FROM_USD_TO_KRW)을 어디서 관리할지 고민이 필요함.

--- BE/src/main/java/com/codesquad/airbnb/business/FilterService.java
+++ BE/src/main/java/com/codesquad/airbnb/business/FilterService.java
@@ -1,9 +1,10 @@
 package com.codesquad.airbnb.business;
 
+import com.codesquad.airbnb.common.DAOUtils;
 import com.codesquad.airbnb.dao.AccommodationDAO;
+import com.codesquad.airbnb.dao.FeePolicyMapper;
 import com.codesquad.airbnb.domain.dto.AccommodationDTO;
 import com.codesquad.airbnb.domain.model.Filter;
-import com.codesquad.airbnb.util.CurrencyConvertor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -17,43 +18,37 @@ public class FilterService {
 
     private static final Logger log = LoggerFactory.getLogger(FilterService.class);
     private AccommodationDAO accommodationDAO;
+    private FeePolicyMapper feePolicyMapper;
 
-    public FilterService(AccommodationDAO accommodationDAO) {
+    public FilterService(AccommodationDAO accommodationDAO, FeePolicyMapper feePolicyMapper) {
         this.accommodationDAO = accommodationDAO;
+        this.feePolicyMapper = feePolicyMapper;
     }
 
     public Map<String, Object> getAccommodations(Filter filter) {
         log.debug("period: {}", filter.getPeriod());
 
-        Map<String, Object> parameters = createParameters(filter);
+        Map<String, Object> parameters = DAOUtils.createParameters(filter);
         Map<String, Object> result = new HashMap<>();
+
+        float accommodationTax = feePolicyMapper.findAccommodationTax();
 
         result.put("accommodations", accommodationDAO.findUsingFilter(parameters)
                 .stream()
                 .map(model -> new AccommodationDTO.Builder(model.getId())
                         .name(model.getName())
-                        .pricePerNight(CurrencyConvertor.calculateCurrency(model.getPricePerNight()))
-                        .pricePerNightDiscounted(CurrencyConvertor.calculateCurrency(model.getPricePerNightDiscounted()))
-                        .totalPrice(CurrencyConvertor.calculateCurrency(model.getTotalPrice() * filter.getPeriod()))
+                        .pricePerNight(model.getPricePerNight(accommodationTax))
+                        .pricePerNightDiscounted(model.getPricePerNightDiscounted(accommodationTax))
+                        .totalPrice(model.getTotalPrice(filter, accommodationTax))
                         .isSuperHost(model.isSuperHost())
                         .city(model.getCity().getName())
                         .scoresRating(model.getScoresRating())
                         .images(model.getImages())
-                        .build()).collect(Collectors.toList()));
+                        .build())
+                .collect(Collectors.toList()));
 
         result.put("total", accommodationDAO.countOfFilterResult(parameters));
 
         return result;
-    }
-
-    private Map<String, Object> createParameters(Filter filter) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("people", filter.getPeople());
-        parameters.put("priceMin", filter.getPriceMin());
-        parameters.put("priceMax", filter.getPriceMax());
-        parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
-        parameters.put("period", filter.getPeriod());
-        parameters.put("itemsOffset", filter.getItemsOffset());
-        return parameters;
     }
 }
--- BE/src/main/java/com/codesquad/airbnb/util/CurrencyConvertor.java
+++ BE/src/main/java/com/codesquad/airbnb/util/CurrencyConvertor.java
@@ -3,8 +3,4 @@ package com.codesquad.airbnb.util;
 public class CurrencyConvertor {
 
     public static final float EXCHANGE_RATE_FROM_USD_TO_KRW = 1231.54f;
-
-    public static int calculateCurrency(float dollar) {
-        return (int)(dollar * EXCHANGE_RATE_FROM_USD_TO_KRW);
-    }
 }
