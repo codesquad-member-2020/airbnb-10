14c85cc0526a09ea54587effc9ebd08a0e5cd8f9
Jay <beginin15@gmail.com>
2020-06-03 00:15:56 +0900

    [#45] feat: 예약 관련 api를 처리하는 BookController 클래스 정의

---
+++ BE/src/main/java/com/codesquad/airbnb/web/production/BookController.java
@@ -0,0 +1,28 @@
+package com.codesquad.airbnb.web.production;
+
+import com.codesquad.airbnb.business.BookService;
+import com.codesquad.airbnb.domain.dto.AccommodationDTO;
+import com.codesquad.airbnb.domain.model.Filter;
+import com.codesquad.airbnb.web.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> getReservationInfo(@PathVariable Integer id, Filter filter) {
+        return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.SUCCESS, bookService.getAccommodation(id, filter)), HttpStatus.OK);
+    }
+}
