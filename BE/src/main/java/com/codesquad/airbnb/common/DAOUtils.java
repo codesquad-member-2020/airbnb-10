105a7eb2c9deb7f0f545c63d5e0e82dc764fa3d3
Jay <beginin15@gmail.com>
2020-06-03 00:05:36 +0900

    [#45] feat: BookService 클래스 정의 및 예약 모달창 정보를 가져오는 메소드 추가
    - DAOUtils.java: FilterService와 BookService에서 중복되는 코드를 처리하는 유틸 클래스 정의
    
    - AccommodationDTO.java: 기간에 대한 요금 계산 시, LocalDate 날짜 차이값이 long 타입이므로 이를 곱한 요금도 long 타입으로 변경함

---
+++ BE/src/main/java/com/codesquad/airbnb/common/DAOUtils.java
@@ -0,0 +1,28 @@
+package com.codesquad.airbnb.common;
+
+import com.codesquad.airbnb.domain.model.Filter;
+import com.codesquad.airbnb.util.CurrencyConvertor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DAOUtils {
+
+    public static Map<String, Object> createParameters(Filter filter) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("people", filter.getPeople());
+        parameters.put("priceMin", filter.getPriceMin());
+        parameters.put("priceMax", filter.getPriceMax());
+        parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
+        parameters.put("period", filter.getPeriod());
+        parameters.put("itemsOffset", filter.getItemsOffset());
+        return parameters;
+    }
+
+    public static Map<String, Object> createParameters(Filter filter, Integer id) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("exchangeRate", CurrencyConvertor.EXCHANGE_RATE_FROM_USD_TO_KRW);
+        parameters.put("id", id);
+        return parameters;
+    }
+}
