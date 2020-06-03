1ed695462f93d26b7ffd733e204313548130bcf0
최장호 <hmchoi651@naver.com>
2020-06-03 20:43:52 +0900

    [#47] Feat : useRoomsFetch Custom hook 생성
    초기에 필터링을 설정하지 않았을 시 , 새로고침이 될 때 , 필터링 옵션이 변경될 때를 분기 처리 해서 fetch url을 변경시키는 Custom hook 추가

--- FE/airbnb/src/hooks/useFetch.jsx
+++ FE/airbnb/src/hooks/useFetch.jsx
@@ -1,5 +1,10 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { faLongArrowAltUp } from "@fortawesome/free-solid-svg-icons";
+import { resetPrices } from "../modules/price";
+import querystring from "query-string";
+import _ from "../util/util.js";
+import useUpdateStore from "./useUpdateStore.jsx";
 
 const useFetch = (url, actionFunc) => {
   const dispatch = useDispatch();
@@ -10,7 +15,7 @@ const useFetch = (url, actionFunc) => {
       const initialData = await response.json();
       dispatch(actionFunc(initialData));
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -23,6 +28,36 @@ export const fetchData = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
+};
+
+export const useRoomsFetch = (actionFunc, searchQuery) => {
+  const dispatch = useDispatch();
+  const { startDate } = useSelector((state) => state.dateReducer);
+  const ROOMS_DB_HOST = process.env.REACT_APP_ROOMS_DB_HOST;
+
+  let requsetUrl = null;
+
+  if (!searchQuery) {
+    //query string none
+    requsetUrl = _.createInitialRequsetURL();
+  } else if (searchQuery && !startDate) {
+    // query string Exist store state none
+    const parsed = querystring.parse(searchQuery);
+    requsetUrl = ROOMS_DB_HOST + searchQuery;
+    useUpdateStore(dispatch, parsed);
+  } else {
+    // query string Exist store state Exist
+    requsetUrl = ROOMS_DB_HOST + searchQuery;
+  }
+  useEffect(() => {
+    fetchRooms();
+  }, [searchQuery]);
+
+  const fetchRooms = async () => {
+    const response = await fetch(requsetUrl);
+    const data = await response.json();
+    dispatch(actionFunc(data));
+  };
 };
 
 export default useFetch;
