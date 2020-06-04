6f057fdde895e0574adc77867a672555b8dcfbd8
최장호 <hmchoi651@naver.com>
2020-06-04 16:49:10 +0900

    [#47] Feat : pagination이 발생하면 roomsList Store에 accommodations의 값을 최신으로 업데이트 시켜주는 로직 구현

--- FE/airbnb/src/modules/roomsList.js
+++ FE/airbnb/src/modules/roomsList.js
@@ -1,9 +1,17 @@
 const INITIAL_FETCH = "roomsList/INITIAL_FETCH";
+const PAGINATION_FETCH = "roomsList/PAGINATION_FETCH";
 
 export const fetchInitialData = (initialData) => {
   return {
     type: INITIAL_FETCH,
     initialData: initialData.content,
+  };
+};
+
+export const fetchPaginationDate = (data) => {
+  return {
+    type: PAGINATION_FETCH,
+    accommodations: data.content.accommodations,
   };
 };
 
@@ -39,6 +47,12 @@ const roomsListReducer = (state = initialValue, action) => {
   switch (action.type) {
     case INITIAL_FETCH:
       return { ...state, content: action.initialData };
+    case PAGINATION_FETCH:
+      return {
+        ...state,
+        content: { ...state.content, accommodations: action.accommodations },
+      };
+
     default:
       return state;
   }
