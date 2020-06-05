7dddc717ae00f060bc1e1356fb92cda2a0bf55a3
Jay <beginin15@gmail.com>
2020-06-05 12:59:49 +0900

    [#28] feat: 컨트롤러 예약하기 메소드 구현, 예약 관련 메소드에서 존재하지 않는 엔티티에 대한 요청 예외 처리 로직 추가

--- BE/src/main/java/com/codesquad/airbnb/web/production/BookController.java
+++ BE/src/main/java/com/codesquad/airbnb/web/production/BookController.java
@@ -1,14 +1,15 @@
 package com.codesquad.airbnb.web.production;
 
 import com.codesquad.airbnb.business.BookService;
+import com.codesquad.airbnb.common.utils.CustomValidatorUtils;
 import com.codesquad.airbnb.domain.model.Filter;
+import com.codesquad.airbnb.domain.model.User;
+import com.codesquad.airbnb.domain.validator.FilterValidator;
 import com.codesquad.airbnb.web.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rooms")
@@ -21,7 +22,30 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getReservationInfo(@PathVariable Integer id, Filter filter) {
-        return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.SUCCESS, bookService.getAccommodation(id, filter)), HttpStatus.OK);
+    public ResponseEntity<ApiResponse> getReservationInfo(@PathVariable Integer id, Filter filter, BindingResult result) {
+        new FilterValidator().validate(filter, result);
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.FAIL,
+                    CustomValidatorUtils.getErrorMessage(result)), HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.SUCCESS,
+                bookService.getAccommodation(id, filter)), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResponse> booking(@PathVariable Integer id, Filter filter, BindingResult result) {
+        new FilterValidator().validate(filter, result);
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.FAIL,
+                    CustomValidatorUtils.getErrorMessage(result)), HttpStatus.FORBIDDEN);
+        }
+
+        if (bookService.booking(id, filter, new User(2, "beginin@gmail.com"))) {
+            return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.SUCCESS,
+                    null), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new ApiResponse(ApiResponse.Status.FAIL,
+                "이미 예약된 숙소입니다."), HttpStatus.FORBIDDEN);
     }
 }
