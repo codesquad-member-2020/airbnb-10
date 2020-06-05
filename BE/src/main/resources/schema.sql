7b68f6eff683a1922f42a70c33adb99f082fcab9
Jay <beginin15@gmail.com>
2020-06-05 12:42:43 +0900

    [#28] refactor: booking 컬럼에 NOT NULL 제약 조건 추가

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS booking;
 
 CREATE TABLE booking (
     id INT AUTO_INCREMENT,
-    user INT,
-    accommodation INT,
-    check_in DATE,
-    check_out DATE,
+    user INT NOT NULL,
+    accommodation INT NOT NULL,
+    check_in DATE NOT NULL,
+    check_out DATE NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user) REFERENCES user(id),
     FOREIGN KEY (accommodation) REFERENCES accommodation(id)
