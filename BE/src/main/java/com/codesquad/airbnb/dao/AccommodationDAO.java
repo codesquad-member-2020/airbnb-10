dbb696d7a0b22b9676e12abdd7db026a28ef3a07
Jay <beginin15@gmail.com>
2020-06-07 17:31:06 +0900

    [#55] refactor: sqlSession 없이 메소드와 xml 파일 연동하기

--- BE/src/main/java/com/codesquad/airbnb/dao/AccommodationDAO.java
+++ BE/src/main/java/com/codesquad/airbnb/dao/AccommodationDAO.java
@@ -1,44 +1,23 @@
 package com.codesquad.airbnb.dao;
 
 import com.codesquad.airbnb.domain.model.Accommodation;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
 
-@Repository
-public class AccommodationDAO {
+@Mapper
+public interface AccommodationDAO {
 
-    private static final String NAMESPACE = "accommodationMapper.";
+    public List<Accommodation> findUsingFilter(Map<String, Object> parameters);
 
-    private SqlSession sqlSession;
+    public int countOfFilterResult(Map<String, Object> parameters);
 
-    public AccommodationDAO(SqlSession sqlSession) {
-        this.sqlSession = sqlSession;
-    }
+    public List<Integer> findFeeUsingFilter(Map<String, Object> parameters);
 
-    public List<Accommodation> findUsingFilter(Map<String, Object> parameters) {
-        return sqlSession.selectList(NAMESPACE + "findUsingFilter", parameters);
-    }
+    public List<Map<String, Integer>> findFeeUsingFilterTest(Map<String, Object> parameters);
 
-    public int countOfFilterResult(Map<String, Object> parameters) {
-        return sqlSession.selectOne(NAMESPACE + "countOfFilterResult", parameters);
-    }
+    public Accommodation findAccommodationChargeInfoById(Map<String, Object> parameters);
 
-    public List<Integer> findFeeUsingFilter(Map<String, Object> parameters) {
-        return sqlSession.selectList(NAMESPACE + "findFeeUsingFilter", parameters);
-    }
-
-    public List<Map<String, Integer>> findFeeUsingFilterTest(Map<String, Object> parameters) {
-        return sqlSession.selectList(NAMESPACE + "findFeeUsingFilterForTest", parameters);
-    }
-
-    public Accommodation findAccommodationChargeInfoById(Map<String, Object> parameters) {
-        return sqlSession.selectOne(NAMESPACE + "findAccommodationChargeInfoById", parameters);
-    }
-
-    public Map<String, Integer> findMinAndMaxOfFee(Map<String, Object> parameters) {
-        return sqlSession.selectOne(NAMESPACE + "findMinAndMaxOfFee", parameters);
-    }
+    public Map<String, Integer> findMinAndMaxOfFee(Map<String, Object> parameters);
 }
